72421c41be3507d9abc2e7780f441b9285cbb8d6
Riya Krishnan <67266080+RiyaKrishnan@users.noreply.github.com>
2020-06-26 12:44:46 +0530

    Add files via upload

---
+++ Directories.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#creating directory and accessing element
+Dict= dict({1: 'my', 2: 'name' , 3: 'is' , 4: 'riya'})
+print (Dict[2])
+
+
+# In[3]:
+
+
+#creating directory and using pop()
+Dict= {1: 'what' , 2: 'why'}
+print(Dict.pop(2))
+
+
+# In[4]:
+
+
+#creating nested directory
+Dict= {}
+Dict[1]= 'name'
+Dict[2]= 'is' , 'riya'
+print (Dict)
+
+
+# In[ ]:
+
+
+
+
