ada84ed628c485c2b6e9b8f334daf8eaa214f9b5
Riya Krishnan <67266080+RiyaKrishnan@users.noreply.github.com>
2020-08-04 13:42:59 +0530

    Add files via upload

---
+++ Practice programs.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#combining 2 directories
+rpcs_attended={'name1':'edwin','name2':'riya'}
+rpcs_absent= {'absent1':'girish','absent2':'kamlesh'}
+total_students=[rpcs_attended,rpcs_absent]
+for students in total_students:
+        print(total_students)    
+
+
+# In[10]:
+
+
+#adding element to a dirc
+rpcs_attended={'name1': 'edwin','name2':'riya'}
+rpcs_attended['name3']='girish'
+print(rpcs_attended)
+
+
+# In[11]:
+
+
+#preparing directory
+rpcs_attended={'nam1':'edwin','name2':'riya'}
+print(rpcs_attended)
+
+
+# In[13]:
+
+
+#using if
+price=10
+if price<15:
+    print("you have enough money")
+
+
+# In[15]:
+
+
+#using if else
+age=2
+if age<5:
+    print("he/she can be admitted")
+else:
+    print("\n if he /she is above 5, cannot be admitted")
+
+
+# In[18]:
+
+
+#using elif
+age=4
+if age>4:
+    print("he/she may be admitted")
+elif age==4:
+    print("\n if he/she is 4,they can be interviewed")
+else:
+    print("not possible to admit")
+
+
+# In[23]:
+
+
+#using if else with list
+list1=[2,4,5,8,10,15,23,25,30,41,55,65]
+for i in list1:
+    if(i % 5 == 0)
+     print(i)
+    else:
+       print("no number is divisible by 5") 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
